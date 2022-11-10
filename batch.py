eb2409aea8c07d2094df7b654b804d061d55736f
endaaman <endaaman@frontier.hokudai.ac.jp>
2022-11-10 16:31:18 +0900

    update

--- batch.py
+++ batch.py
@@ -16,9 +16,13 @@ from torch.utils.data import Dataset
 import pydicom
 from openslide import OpenSlide
 
-from endaaman import Commander, curry, with_wrote
+from endaaman import Commander, curry, with_log
 
 Image.MAX_IMAGE_PIXELS = 1000000000
+
+J = os.path.join
+
+with_wrote = lambda s: with_log(s, 'wrote {}')
 
 
 class C(Commander):
@@ -33,20 +37,30 @@ class C(Commander):
             print(f'save {level} {d}')
 
     def arg_thres(self, parser):
+        parser.add_argument('--file', '-f', required=True)
         parser.add_argument('--level', '-l', type=int, default=3)
+        parser.add_argument('--dilate', '-d', type=int, default=10)
 
     def run_thres(self):
-        wsi = cv2.imread(f'tmp/wsi_{self.args.level}.jpg')
+        o = OpenSlide(self.args.file)
+        name = os.path.splitext(os.path.basename(self.args.file))[0]
+        dim = o.level_dimensions[self.args.level]
+        original = o.get_thumbnail(dim)
+
+        wsi = cv2.cvtColor(np.array(original), cv2.COLOR_RGB2BGR)
         gray = cv2.cvtColor(wsi, cv2.COLOR_BGR2GRAY)
         thres = cv2.adaptiveThreshold(
             gray, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
             cv2.THRESH_BINARY, 51, 20)
 
-        cv2.imwrite(with_wrote(f'tmp/thres_{self.args.level}.jpg'), thres)
+        out_dir = with_log(f'out/crop/{name}', 'mkdir {}')
+        os.makedirs(out_dir, exist_ok=True)
 
-        krn = np.ones((10, 10), np.uint8)
+        cv2.imwrite(with_wrote(J(out_dir, f'{self.args.level}_thres.jpg')), thres)
+
+        krn = np.ones((self.args.dilate, self.args.dilate), np.uint8)
         dialated = cv2.dilate(cv2.bitwise_not(thres), kernel=krn, iterations=1)
-        cv2.imwrite(with_wrote(f'tmp/dialated_{self.args.level}.jpg'), dialated)
+        cv2.imwrite(with_wrote(J(out_dir, f'{self.args.level}_dialated.jpg')), dialated)
 
         # contours, hierarchy = cv2.findContours(dialated, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
         contours, hierarchy = cv2.findContours(dialated, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
@@ -54,14 +68,14 @@ class C(Commander):
 
         for i in range(0, len(contours)):
             if len(contours[i]) > 0:
-                # remove small objects
-                if cv2.contourArea(contours[i]) < 100*100:
+                # remove small objects(b
+                if cv2.contourArea(contours[i]) < (min(dim) / 20) ** 2:
                     continue
                 rect = contours[i]
                 x, y, w, h = cv2.boundingRect(rect)
                 cv2.rectangle(wsi, (x, y), (x + w, y + h), (0, 0, 0), 10)
 
-        cv2.imwrite(with_wrote(f'tmp/result_{self.args.level}.jpg'), wsi)
+        cv2.imwrite(with_wrote(J(out_dir, f'{self.args.level}_result.jpg')), wsi)
 
         self.contours, self.hierarchy = contours, hierarchy
         self.wsi = wsi
